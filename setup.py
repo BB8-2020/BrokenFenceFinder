effd6d8d48ad397cf85fb17d1254dced13b995f4
David Demmers <daviddemmers@gmail.com>
2021-03-24 12:04:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+  
+from setuptools import find_packages, setup
 
+install_requires = [
+]
+
+setup(
+    name='BrokenFenceFinder',
+    version='0.0',
+    description='Broken fence finder model for parrot drones',
+    author='David Demmers',
+    author_email='david.demmers@student.hu.nl',
+    packages=find_packages(),
+    install_requires=install_requires,
+    dependency_links=[
+        "git+https://github.com/BB8-2020/BrokenFenceFinder",
+    ],
+)
