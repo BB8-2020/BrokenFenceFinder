8fc5f4eb46989f2eff424be1d8bbcd78960da5d9
Floris Videler <floris.videler@student.hu.nl>
2021-04-07 08:58:11 +0200

    Added takeoff demo code AB#49895

--- drone_automation/takeoff.py
+++ drone_automation/takeoff.py
@@ -4,15 +4,14 @@ import olympe
 import time
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing
 
-DRONE_IP = "10.202.0.1"
+DRONE_IP = "192.168.42.1"
+
+olympe.log.update_config({"loggers": {"olympe": {"level": "INFO"}}})
 
 
 def main():
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
-    assert drone(TakeOff()).wait().success()
-    time.sleep(10)
-    assert drone(Landing()).wait().success()
     drone.disconnect()
 
 
